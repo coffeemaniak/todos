1f29dd8cc9e11a4333430f5e2876f5965894a381
coffeemaniak <derrykain@mail.ru>
2021-04-01 18:04:50 +0300

    first

---
+++ src/header/header.js
@@ -0,0 +1,13 @@
+import {React, Component} from "react";
+import "./header.css";
+
+const Header = () => {
+ return (
+     <div className="header">
+         <h1 className="header_title">My Classic ToDoS</h1>
+        
+     </div>
+ )
+}
+
+export default Header;
